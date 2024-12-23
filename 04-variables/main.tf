34ed0f17144091c713e8a588e088c9b6209ee2b8
venkat <devops.bheemireddy@gmail.com>
2024-12-23 21:36:12 +1100

    Roboshop project changes v2.0

--- 04-variables/main.tf
+++ 04-variables/main.tf
@@ -1,18 +1,36 @@
-### Plain Variable
+variable "s1" {
+  default = "Hello"
+}
+
+variable "n1" {
+  default = 2
+}
+
+variable "b1" {
+  default = true
+}
+
+
+
+# Data types that terraform supports
+# 1. String
+# 2. Number
+# 3. Boolean
+
+# String only require quotes, THat to be only double quotes. Terraform does not have single quotes support.
+
 variable "v1" {
   default = "Hello"
 }
 
-### List Variable
 variable "v2" {
   default = [
-  "Hello",
-  2,
-  false
+    "Hello",
+    2,
+    false
   ]
 }
 
-### Map Variable
 variable "v3" {
   default = {
     course = "devops"
@@ -20,32 +38,45 @@ variable "v3" {
   }
 }
 
+# Variable Type that terraform supports
+# 1. plain
+# 2. List ( A key having multiple values, And each value can be a different data type)
+# 3. Map ( A key having again keys and values )
 
-### Accessing Variables
+
+## Access your variable
 output "o1" {
   value = var.v1
 }
 
+# While accessing variable, if we have some string combination then we can access it with ${}
 output "o2" {
   value = "${var.v1} - John"
 }
 
+# Access a value of list
 output "o3" {
   value = var.v2[0]
 }
 
+# Access a value of Map
 output "o4" {
   value = var.v3["cloud"]
 }
 
-### Example for terraform.tfvars
+# So far we are hardcoding the values in variables, Most times those come as input
+
+# Values from terraform.tfvars, Terraform picks this file automatically, Other tfvars needs to be parsed
 variable "v10" {}
 output "o10" {
   value = var.v10
 }
 
-### Example for other tfvars
+# We can also access values from other tfvars too, but we need to parse that input
+# ex: dev.tfvars
 variable "env" {}
-output "o11" {
+output "env" {
   value = var.env
 }
+
+# Terraform can pick the files automatically if the files are *.auto.tfvars
