21578b56291eeae15de9e973525cf6a66cf1b333
venkat <devops.bheemireddy@gmail.com>
2024-12-23 21:20:06 +1100

    Roboshop project changes v2.0

--- roboshop-v1/data.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_resource_group" "example" {
-  name = "project-setup-1"
-}
-
-data "azurerm_subnet" "example" {
-  name                 = "default"
-  virtual_network_name = "project-setup-network"
-  resource_group_name  = data.azurerm_resource_group.example.name
-}
