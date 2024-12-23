6b52c358470ba642263e80d6fccbd511d564c5b6
venkat <devops.bheemireddy@gmail.com>
2024-12-23 21:14:30 +1100

    Roboshop project changes v2.0

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_network_interface" "frontend" {
-  name                = "frontend-nic"
+resource "azurerm_network_interface" "main" {
+  name                = "${var.component}-nic"
   location            =  data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
 
@@ -7,15 +7,27 @@ resource "azurerm_network_interface" "frontend" {
     name                          = "internal"
     subnet_id                     = data.azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "${var.component}-ip"
+  location              = data.azurerm_resource_group.example.location
+  resource_group_name   = data.azurerm_resource_group.example.name
+  allocation_method   = "Static"
+
+  tags = {
+    component = var.component
   }
 }
 
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = "frontend"
+  name                  = var.component
   location              = data.azurerm_resource_group.example.location
   resource_group_name   = data.azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
+  network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
@@ -23,20 +35,17 @@ resource "azurerm_virtual_machine" "main" {
 
 
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+    id = "/subscriptions/7b6c642c-6e46-418f-b715-e01b2f871413/resourceGroups/trail1/providers/Microsoft.Compute/galleries/LDOTrail/images/rhel9-devops-practice/versions/04.12.2024"
   }
 
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = var.component
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "hostname"
+    computer_name  = var.component
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }
@@ -44,6 +53,23 @@ resource "azurerm_virtual_machine" "main" {
     disable_password_authentication = false
   }
   tags = {
-    environment = "staging"
+    component = var.component
+  }
+
+
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "testadmin"
+      password = "Password1234!"
+      host     = azurerm_public_ip.main.ip_address
+    }
+
+    inline = [
+      "sudo dnf install python3.12-pip -y",
+      "sudo pip3.12 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/raghudevopsb82/roboshop-ansible roboshop.yml -e app_name=${var.component} -e ENV=dev"
+    ]
   }
 }
