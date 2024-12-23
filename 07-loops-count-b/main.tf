a19256074dec83866c3ff6e596867fda3c700ab2
venkat <devops.bheemireddy@gmail.com>
2024-12-23 17:31:06 +1100

    Roboshop project changes v2.0

--- 07-loops-count-b/main.tf
+++ 07-loops-count-b/main.tf
@@ -1,9 +1,24 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.1"
+    }
+  }
+}
+
 resource "null_resource" "demo" {
   count = length(var.demo)
 
   provisioner "local-exec" {
     command = "echo ${var.demo[count.index]}"
   }
+}
+
+resource "local_file" "foo" {
+  count = length(var.demo)
+  content = var.demo[count.index]
+  filename = "/tmp/file-${count.index}"
 }
 
 variable "demo" {
