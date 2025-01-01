4108b610af3f25b1410a8c688f65dcab32c330d8
venkat <devops.bheemireddy@gmail.com>
2025-01-01 17:47:37 +1100

    terraform project

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_virtual_machine" "main" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = var.vm_name
+    name              = "${var.vm_name}.-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
