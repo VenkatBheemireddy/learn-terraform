d1c3b5ab61b0a23ec422f84c940134b2dd6d0db5
venkat <devops.bheemireddy@gmail.com>
2025-01-02 13:23:09 +1100

    terraform project

--- roboshop-v1/vm/main.tf
+++ roboshop-v1/vm/main.tf
@@ -1,5 +1,14 @@
+### Create public ip address for each component
+resource "azurerm_public_ip" "main" {
+  name                  = "${var.component}-ip"
+  location              = data.azurerm_resource_group.example.location
+  resource_group_name   = data.azurerm_resource_group.example.name
+  allocation_method     = "Static"
+}
+
+### Create network interface for each component
 resource "azurerm_network_interface" "main" {
-  name                = "${var.component}.-nic"
+  name                = "${var.component}-nic"
   location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
 
@@ -7,41 +16,65 @@ resource "azurerm_network_interface" "main" {
     name                          = "internal"
     subnet_id                     = data.azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.main.id
+    public_ip_address_id          = azurerm_public_ip.main.id
   }
 }
 
-resource "azurerm_public_ip" "main" {
-  name                = "${var.component}.-ip"
-  location              = data.azurerm_resource_group.example.location
-  resource_group_name   = data.azurerm_resource_group.example.name
-  allocation_method   = "Static"
+### Create network interface security group association
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
 
-  tags = {
-    component = var.component
+### Create dns record for each component
+resource "azurerm_dns_a_record" "main" {
+  name                =  var.component   # I removed "dev" here
+  zone_name           = "azdevopsv82.online"
+  resource_group_name = data.azurerm_resource_group.example.name
+  ttl                 = 10
+  records             = [azurerm_network_interface.main.private_ip_address]
+}
+
+### Create network security group for each component
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.component}-nsg"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "main"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
+### Create virtual machine for each component
 resource "azurerm_virtual_machine" "main" {
+  depends_on            = [azurerm_network_interface_security_group_association.main, azurerm_dns_a_record.main]
   name                  = var.component
   location              = data.azurerm_resource_group.example.location
   resource_group_name   = data.azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_B2s"
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # Delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # Delete the data disks automatically when deleting the VM
   delete_data_disks_on_termination = true
 
+  # This should be your own azure gallery image
   storage_image_reference {
     id = "/subscriptions/9af0e83a-d3ee-4c3c-a244-3274a3457024/resourceGroups/project-setup-1/providers/Microsoft.Compute/galleries/CustomPractice/images/CustomImage"
-    # publisher = "Canonical"
-    # offer     = "0001-com-ubuntu-server-jammy"
-    # sku       = "22_04-lts"
-    # version   = "latest"
   }
+
+  # Create disk for each component
   storage_os_disk {
     name              = var.component
     caching           = "ReadWrite"
@@ -50,26 +83,38 @@ resource "azurerm_virtual_machine" "main" {
   }
   os_profile {
     computer_name  = var.component
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
+    admin_username = "venkat"
+    admin_password = "Devops123456"
   }
   os_profile_linux_config {
     disable_password_authentication = false
   }
-  tags = {
-    component = var.component
+}
+
+### Running provisioner outside virtual machine code block for each component
+### Else it will destroy and re-create the resources when we perform terraform apply
+resource "null_resource" "ansible" {
+
+  # This is to hold provisioner until the vm gets created
+  depends_on = [azurerm_virtual_machine.main]
+
+  # To run the provisioner on remote machine we are using remote-exec
+  provisioner "remote-exec" {
+
+    # To establish connection to remote machine
+    connection {
+      type     = "ssh"
+      user     = "venkat"
+      password = "Devops123456"
+      host     = azurerm_public_ip.main.ip_address
+    }
+
+    inline = [
+      "sudo dnf install python3.12-pip -y",
+      "sudo pip3.12 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/VenkatBheemireddy/roboshop-ansible.git roboshop.yml -e appl=${var.component}"
+      # Commented Raghu Code
+      # "ansible-pull -i localhost, -U https://github.com/raghudevopsb82/roboshop-ansible roboshop.yml -e app_name=${var.component} -e ENV=dev"
+    ]
   }
 }
-# provisioner "remote-exec" {
-#   connection {
-#     type = "ssh"
-#     user = "testadmin"
-#     password = "Password1234!"
-#     host = azurerm_public_ip.main.ip_address
-#   }
-#   inline = [
-#     "sudo dnf install python3.12-pip -y",
-#     "sudo pip3.12 install ansible",
-#     "ansible-pull -i localhost, -U https://github.com/VenkatBheemireddy/roboshop-ansible.git roboshop.yml -e app_name=${var.component} -e ENV=dev"
-#   ]
-# }
