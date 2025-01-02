6586f35d58ed22c892f34c9cea9b572ac2412696
venkat <devops.bheemireddy@gmail.com>
2025-01-03 10:58:20 +1100

    terraform project

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -6,7 +6,19 @@ output "test" {
 
 # These conditions with the combination of loop can be used to determine whether a
 # resource to run or not
-
 resource "null_resource" "test" {
   count = var.a > 10 ? 1 : 0
+}
+
+# If the above expression var.a > 10 ? 1 : 0, lets us say it needs to be used in
+# 10 different places, tomorrow if we want to change the expression, it becomes
+# a duplicate work to change in 10 places, to keep the code dry, terraform supports
+# local resources (which is like run time variables).
+
+locals {
+  count = var.a > 10 ? 1 : 0
+}
+
+resource "null_resource" "test1" {
+  count = local.count
 }
