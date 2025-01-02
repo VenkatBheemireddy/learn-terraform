650e82797e75a18e8ecceb2808497e56ec5052fc
venkat <devops.bheemireddy@gmail.com>
2025-01-03 10:42:30 +1100

    terraform project

---
+++ 09-conditions/main.tf
@@ -0,0 +1,12 @@
+variable "a" {}
+
+output "test" {
+  value = var.a > 10 ? "a is greater than 10" : "a is lesser than 10"
+}
+
+# These conditions with the combination of loop can be used to determine whether a
+# resource to run or not
+
+resource "null_resource" "test" {
+  count = var.a > 10 ? 1 : 0
+}
