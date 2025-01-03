e797c016a3c080b6b7e450699deb0a057d4a70db
venkat <devops.bheemireddy@gmail.com>
2025-01-03 15:09:28 +1100

    terraform project

--- 10-remote-state/main.tf
+++ 10-remote-state/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "project-setup-1"
     storage_account_name = "v82tfstates"
-    container_name       = "tfstate"
+    container_name       = "tfstates"
     key                  = "example.tfstate"
   }
 }
