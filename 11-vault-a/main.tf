4e874943365bb39108823a770c274312f17041e0
venkat <devops.bheemireddy@gmail.com>
2025-01-04 22:45:40 +1100

    terraform project

--- 11-vault-a/main.tf
+++ 11-vault-a/main.tf
@@ -1,17 +1,9 @@
 provider "vault" {
-  # It is strongly recommended to configure this provider through the
-  # environment variables described above, so that each user can have
-  # separate credentials set in the environment.
-  #
-  # This will default to using $VAULT_ADDR
-  # But can be set explicitly
   address = "http://vault.azdevopsv82.online:8200"
   token   = var.token
 }
 
 variable "token" {}
-
-### Method - 1 to get the secret using local_file resource
 
 data "vault_kv_secret" "secret_data" {
   path = "test/data/demo-ssh"
@@ -19,14 +11,6 @@ data "vault_kv_secret" "secret_data" {
 
 resource "local_file" "full" {
   filename = "/tmp/pass"
-  content = data.vault_kv_secret.secret_data.data
-  #content = replace(replace(jsonencode(data.vault_kv_secret.secret_data), "\"", ""), ":", "=")
+# content = data.vault_kv_secret.secret_data.data
+  content = replace(replace(jsonencode(data.vault_kv_secret.secret_data), "\"", ""), ":", "=")
 }
-
-# Execution:
-# git pull
-# terraform init
-# terraform apply -auto-approve -var token=<root-pwd>
-# Output: Along with the Password, this output will give more information
-
-
--- 11-vault-b/main.tf
+++ 11-vault-b/main.tf
@@ -1,17 +1,9 @@
 provider "vault" {
-  # It is strongly recommended to configure this provider through the
-  # environment variables described above, so that each user can have
-  # separate credentials set in the environment.
-  #
-  # This will default to using $VAULT_ADDR
-  # But can be set explicitly
   address = "http://vault.azdevopsv82.online:8200"
   token   = var.token
 }
 
 variable "token" {}
-
-###Method - 2 to get the secret.
 
 data "vault_generic_secret" "secret_data" {
   path = "test/demo-ssh"
@@ -21,13 +13,6 @@ resource "local_file" "local" {
   filename = "/tmp/pass"
   content  = data.vault_generic_secret.secret_data.data["password"]
 }
-
-# Execution:
-# git pull
-# terraform init
-# terraform apply -auto-approve -var token=<root-pwd>
-# Output: Only the password will be printed
-
 
 
 
