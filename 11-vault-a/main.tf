d236cb065e4fce2e2b46134dda91a43074f170bd
venkat <devops.bheemireddy@gmail.com>
2025-01-04 22:15:16 +1100

    terraform project

---
+++ 11-vault-a/main.tf
@@ -0,0 +1,33 @@
+provider "vault" {
+  # It is strongly recommended to configure this provider through the
+  # environment variables described above, so that each user can have
+  # separate credentials set in the environment.
+  #
+  # This will default to using $VAULT_ADDR
+  # But can be set explicitly
+  address = "http://vault.azdevopsv82.online:8200"
+  token   = var.token
+}
+
+variable "token" {}
+
+# Method - 1 to get the secret.
+
+data "vault_kv_secret" "secret_data" {
+  path = "test/data/demo-ssh"
+}
+
+resource "local_file" "full" {
+  filename = "/tmp/pass"
+  content = data.vault_kv_secret.secret_data.data
+  #content = replace(replace(jsonencode(data.vault_kv_secret.secret_data), "\"", ""), ":", "=")
+}
+
+# Execution:
+# git pull
+# terraform init
+# terraform apply -auto-approve -var token=hvs.sXaXB22fmRP8zQMpGPRKROEF
+
+# Output: Along with the Password, this output will give more information
+
+
---
+++ 11-vault-b/main.tf
@@ -0,0 +1,29 @@
+provider "vault" {
+  # It is strongly recommended to configure this provider through the
+  # environment variables described above, so that each user can have
+  # separate credentials set in the environment.
+  #
+  # This will default to using $VAULT_ADDR
+  # But can be set explicitly
+  address = "http://vault.azdevopsv82.online:8200"
+  token   = var.token
+}
+
+variable "token" {}
+
+data "vault_generic_secret" "secret_data" {
+  path = "test/demo-ssh"
+}
+
+resource "local_file" "local" {
+  filename = "/tmp/pass"
+  content  = data.vault_generic_secret.secret_data.data["password"]
+}
+
+# Execution:
+# git pull
+# terraform init
+# terraform apply -auto-approve -var token=hvs.sXaXB22fmRP8zQMpGPRKROEF
+
+# Output: Only the password will be printed
+
