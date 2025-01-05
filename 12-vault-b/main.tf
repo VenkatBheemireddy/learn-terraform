528eacf8ad94392edbc57074dcf5f509b2ab3f2c
venkat <devops.bheemireddy@gmail.com>
2025-01-05 11:58:24 +1100

    vault coding changes

--- 12-vault-b/main.tf
+++ 12-vault-b/main.tf
@@ -1,19 +1,9 @@
 provider "vault" {
-  # It is strongly recommended to configure this provider through the
-  # environment variables described above, so that each user can have
-  # separate credentials set in the environment.
-  #
-  # This will default to using $VAULT_ADDR
-  # But can be set explicitly
   address = "http://vault.azdevopsv82.online:8200"
   token   = var.token
 }
 
 variable "token" {}
-
-# data "vault_kv_secret" "secret_data" {
-#   path = "test/data/demo-ssh"
-# }
 
 data "vault_generic_secret" "secret_data" {
   path = "test/demo-ssh"
@@ -23,10 +13,3 @@ resource "local_file" "local" {
   filename = "/tmp/pass"
   content  = data.vault_generic_secret.secret_data.data["password"]
 }
-
-
-# resource "local_file" "full" {
-#   filename = "/tmp/pass"
-#   #content = data.vault_kv_secret.secret_data.data
-#   content = replace(replace(jsonencode(data.vault_kv_secret.secret_data), "\"", ""), ":", "=")
-# }
